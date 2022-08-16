f3fc65f8b5d31e85cacfaa32951e407919dc97f3
Igor <iigorfelipe@gmail.com>
2022-08-16 19:38:00 -0300

    ✨ feat: server.ts makes the connection with the bank

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,15 @@
-import express from "express";
+import express, { json } from 'express';
+import router from './routes';
+import db from './database/db';
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Gomu gomu no...");
-});
+app.use(json());
+app.use(router);
 
-app.listen(process.env.PORT, () => console.log(`Online na porta ${process.env.PORT}`));
+app.listen(process.env.PORT, async () => {
+  await db
+    .sync()
+    .then(() => console.log(`Running on: http://localhost:${process.env.PORT}`))
+    .catch((err) => console.log('Unable to connect to the database:', err));
+});
