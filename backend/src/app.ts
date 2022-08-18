ea0052540590636bac97f169101ea97303f8768a
Igor <iigorfelipe@gmail.com>
2022-08-17 22:37:52 -0300

    ♻️ refactor: turns the server into a typed class

---
+++ backend/src/app.ts
@@ -0,0 +1,28 @@
+import express from 'express';
+import router from './routes';
+import db from './database/db';
+
+class App {
+  public app: express.Express;
+
+  constructor() {
+    this.app = express();
+    this.routes();
+  }
+
+  private routes = () => {
+    this.app.use(express.json());
+    this.app.use(router);
+  };
+
+  public start = (PORT: string | number): void => {
+    this.app.listen(PORT, async () => {
+      await db
+        .sync()
+        .then(() => console.log(`Running on: http://localhost:${PORT}`))
+        .catch((err) => console.log('Unable to connect to the database:', err));
+    });
+  };
+}
+
+export default App;
