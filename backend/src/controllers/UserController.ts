59a6342113a5bfcc52b6a50bd882321516e03388
Igor <iigorfelipe@gmail.com>
2022-08-17 22:48:37 -0300

    ♻️ refactor: add type in UserController return and import UserService

--- backend/src/controllers/UserController.ts
+++ backend/src/controllers/UserController.ts
@@ -1,12 +1,17 @@
 import { Request, Response } from 'express';
-import UserModel from '../models/User';
+import UserService from '../services/UserService';
 
 class UserController {
-  create = async (req: Request, res: Response) => {
-    await UserModel
+  private service = new UserService();
+
+  create = async (req: Request, res: Response): Promise<Response> => {
+    const user = await this
+      .service
       .create(req.body)
       .then(() => res.status(200).json({ mensagem: 'Success' }))
       .catch(() => res.status(400).json({ mensagem: 'Failed' }));
+
+    return user;
   };
 }
 
