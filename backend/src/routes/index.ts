966f464f410bc4f694e7d552d551139088f1efcc
Igor <iigorfelipe@gmail.com>
2022-08-19 22:57:01 -0300

    ♻️ refactor: improves indentation and adds middleware routes

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,11 +1,37 @@
 import { Router } from 'express';
 import UserController from '../controllers/UserController';
+import UserMidlewares from '../middlewares/UserMidlewares';
 
 const router = Router();
 
-router.get('/', UserController.findAll);
-router.post('/cadastro', UserController.create);
-router.put('/:id', UserController.update);
-router.delete('/:id', UserController.destroy);
+router.get(
+  '/',
+  UserMidlewares.findAll,
+  UserController.findAll,
+);
+
+router.get(
+  '/:id',
+  UserMidlewares.findOne,
+  UserController.findOne,
+);
+
+router.post(
+  '/cadastro',
+  UserMidlewares.create,
+  UserController.create,
+);
+
+router.put(
+  '/:id',
+  UserMidlewares.update,
+  UserController.update,
+);
+
+router.delete(
+  '/:id',
+  UserMidlewares.findAll,
+  UserController.destroy,
+);
 
 export default router;
